cdd46aecbd91211fac317a7d481b03b123ae4a97
sunitee <suniteebajpai2@gmail.com>
2021-06-04 11:13:59 -0400

    commit tree 31178243caea9c7550d1148fbe50913880e6bedf

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,10 +2,44 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  // const renderMovies = () => {
+  //   return movies.map((movie, index) => {
+  //     return
+  //      <div key= {index}>
+  //         <h1>Name:{movie.title}</h1>
+  //         <h2>Time: {movie.time}</h2>
+  //         <p>Genres</p>
+  //           <ul>
+  //             {movie.genres.map((genre)=> {
+  //               <li> key={index} {genre}</li>
+  //             })}
+  //           </ul>
+  //     </div>
+  //   })
+  // }
   return (
     <div>
-        {/*{code here}*/}
-    </div>
+      <h1>Movies Page</h1>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h3>Name: {movie.title}</h3>
+          <h4>Time: {movie.time}</h4>
+          <p>Genres</p>
+          <ul>
+            {movie.genres.map((genre, index)=>(
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+
+      ))}
+    </div>  
+
+
+
+    
+     
   );
 };
 
